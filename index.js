705e8a73c265bc5fdba70701279a35d561c1d138
Grace de la Mora <grace.g.delamora@gmail.com>
2021-03-10 21:22:48 -0500

    Next group of challenges completed until task 5

--- index.js
+++ index.js
@@ -85,17 +85,30 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(inning, inningCount){
-  /*Code Here*/
+function finalScore(inning, inningCount) {
+  let Home, Away;
+  for (let i = 0; i < inningCount; i++) {
+    Home += inning();
+    Away += inning();
+  }
+  return {
+    Home,
+    Away
+  };
 }
+
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inning) {
+  return {
+    'Home': inning(),
+    'Away': inning()
+  };
 }
 
 
